5936c2d7a11cc3b3986475b943771ab2b2a06e60
Tirdad Abbasi <abbasi.tirdad@gmail.com>
2020-04-09 13:20:12 +0430

    attribute development

---
+++ src/Controllers/AdminUserController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Tir\User\Controllers;
+
+use Tir\User\Entities\User;
+use Tir\Crud\Controllers\CrudController;
+
+class AdminUserController extends CrudController
+{
+    protected $model = User::Class;
+
+}
