5f376fbcc7fc573eb5a92785c48ec22acc31ed84
Tirdad <abbasi.tirdad@gmail.com>
2020-08-03 22:57:13 +0430

    add name to user

--- src/Database/Migrations/2014_10_12_000000_create_users_table.php
+++ src/Database/Migrations/2014_10_12_000000_create_users_table.php
@@ -17,8 +17,9 @@ class CreateUsersTable extends Migration
 
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('first_name');
-            $table->string('last_name');
+            $table->string('name');
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
             $table->integer('user_id');
             $table->string('email')->unique();
             $table->string('password');
