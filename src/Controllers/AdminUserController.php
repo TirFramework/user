0402e0760d3b1a0d6e1076a3df098b0d57f8207d
Tirdad <abbasi.tirdad@gmail.com>
2020-08-05 05:07:54 +0430

    hash user password

--- src/Controllers/AdminUserController.php
+++ src/Controllers/AdminUserController.php
@@ -2,11 +2,26 @@
 
 namespace Tir\User\Controllers;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Tir\User\Entities\User;
 use Tir\Crud\Controllers\CrudController;
 
 class AdminUserController extends CrudController
 {
     protected $model = User::Class;
+
+    public function storeRequestManipulation(Request $request)
+    {
+        //password hashed
+        $password = Hash::make($request->input('password'));
+        $request->merge(['password'=> $password]);
+        return $request;
+    }
+
+    public function updateRequestManipulation(Request $request)
+    {
+        return $this->storeRequestManipulation($request);
+    }
 
 }
